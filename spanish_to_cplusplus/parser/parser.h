7d5c55cc882393913587c506be2ccb08e50a52a2
Moisés Alfonso Terán <teranrivasma@uvm.edu.ve>
2025-03-21 07:40:09 -0400

    tablas mejoradas

--- spanish_to_cplusplus/parser/parser.h
+++ spanish_to_cplusplus/parser/parser.h
@@ -170,7 +170,7 @@ public:
     }
 
     // Método para imprimir tabla de símbolos
-    inline void imprimirTablaSimbolos() const {
+    /*inline void imprimirTablaSimbolos() const {
         std::cout << "\nTabla de Simbolos:\n";
         std::cout << std::left
                 << std::setw(20) << "Nombre"
@@ -183,6 +183,19 @@ public:
                     << std::setw(15) << tipoDatoToString(simbolo.tipo)
                     << std::setw(10) << simbolo.lineaDeclaracion
                     << simbolo.valor << "\n"; // Mostramos el valor
+        }
+    }*/
+    inline void imprimirTablaSimbolos() const {
+        std::cout << "\n\033[1;34mTabla de Simbolos\033[0m\n";
+        std::cout << "\033[1;37m+--------------------+---------------+----------+-------------------+\033[0m\n";
+        std::cout << "\033[1;37m| Nombre             | Tipo          | Linea    | Valor             |\033[0m\n";
+        std::cout << "\033[1;37m+--------------------+---------------+----------+-------------------+\033[0m\n";
+    
+        for (const auto& [nombre, simbolo] : tablaSimbolos.simbolos) {
+            std::cout << "| " << std::setw(18) << nombre
+                      << "| " << std::setw(14) << tipoDatoToString(simbolo.tipo)
+                      << "| " << std::setw(8) << simbolo.lineaDeclaracion
+                      << "| " << std::setw(17) << simbolo.valor << " |\n";
         }
     }
 
