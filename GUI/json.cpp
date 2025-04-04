3b0215c5bfdf1e775c3a6234b026fcfb1f9322dc
Moisés Alfonso Terán <teranrivasma@uvm.edu.ve>
2025-04-04 01:13:33 -0400

    mover archivos de interfaz a GUI

--- GUI/json.cpp
+++ GUI/json.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <fstream>
-#include "nlohmann/json.hpp"
+#include "../lib/nlohmann/json.hpp"
 
 using json = nlohmann::json;
 
--- GUI/main.js
+++ GUI/main.js
@@ -1,7 +1,7 @@
 const { exec } = require("child_process");
 const fs = require("fs");
 
-exec("./json.exe", (error, stdout, stderr) => {
+exec("cd ./out && json.exe", (error, stdout, stderr) => {
     if (error) {
         console.error(`Error: ${error.message}`);
         return;
@@ -13,8 +13,8 @@ exec("./json.exe", (error, stdout, stderr) => {
     console.log("Compilador ejecutado con éxito");
 
     // Leer los archivos generados
-    const jsonData = fs.readFileSync("salida.json", "utf8");
-    const codigoCpp = fs.readFileSync("salida.cpp", "utf8");
+    const jsonData = fs.readFileSync("./out/salida.json", "utf8");
+    const codigoCpp = fs.readFileSync("./out/salida.cpp", "utf8");
 
     console.log("JSON:", jsonData);
     console.log("Código C++:", codigoCpp);
